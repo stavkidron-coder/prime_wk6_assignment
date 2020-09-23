7eb4f7c80b4fa1b357cf5e1cc90037fab9bb64ac
Stav Kidron <kidronstav@gmail.com>
2020-09-23 13:38:51 -0500

    Added input fields and pushed new cars into an array

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -1,5 +1,43 @@
 let garage = [];
 
+let parkedCars = [];
+
+
+// 1. Add an interface for the user to add cars to the garage with 3 input fields:
+//
+// - year
+// - make
+// - model
+//
+// Also an "add car" button that, when clicked, *uses the existing "newCar" function* to add a car to the garage.
+
+$(document).ready(readyNow);
+
+function readyNow(){
+  $('#addCarButton').on('click', addCar);
+} //end readyNow function
+
+
+function addCar(){
+  console.log('in addCar');
+
+  // get user input and create a new object
+  let newCar = {
+    year: $('#carYearIn').val(),
+    make: $('#carMakeIn').val(),
+    model: $('#carModelIn').val()
+  };
+
+  // push the new car into the array
+  parkedCars.push(newCar);
+  // empty the inputs
+  $('#carYearIn').val('');
+  $('#carMakeIn').val('');
+  $('#carModelIn').val('');
+
+} // end addCar function
+
+
 /*
 Do not change newCar for base mode!
 */
@@ -9,7 +47,7 @@ function newCar(yearInput, makeInput, modelInput){
     year: yearInput,
     make: makeInput,
     model: modelInput
-  }
+  };
   garage.push(newCarObject);
   return true;
 } // end newCar
